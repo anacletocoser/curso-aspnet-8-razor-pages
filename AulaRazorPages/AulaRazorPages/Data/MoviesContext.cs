8a410245ab33aa27b51ead0e535112b800b0cab0
Anacleto De Angeli Coser <anacleto.coser@gmail.com>
2024-10-18 17:46:03 -0300

    criação classe context utilizando o EFCore8 InMemory

---
+++ AulaRazorPages/AulaRazorPages/Data/MoviesContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Slugify;
+
+namespace AulaRazorPages.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext() 
+        {
+            this.Database.EnsureCreated();
+        }
+        public MoviesContext(DbContextOptions<MoviesContext> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Movie> Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var slugify = new SlugHelper();
+
+            string[] names = [
+                "O poderoso Chefão",
+                "O senhor dos ANéis: O retorno do Rei",
+                "Interestelar",
+                "O silencio dos inocentes"
+                ];
+
+            int counter = 1;
+            var movies = new List<Movie>();
+            foreach(var name in names)
+            {
+                movies.Add(new Movie
+                {
+                    Id = counter,
+                    Name = name,
+                    Permalink = slugify.GenerateSlug(name)
+                });
+                counter++;
+            }
+            modelBuilder
+                .Entity<Movie>()
+                .HasData(movies);
+        }
+    }
+}
