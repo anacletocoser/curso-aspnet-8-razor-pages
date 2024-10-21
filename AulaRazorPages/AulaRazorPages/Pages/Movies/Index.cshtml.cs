0d4c47340958e7b74490edcc8f472e3910540409
Anacleto De Angeli Coser <anacleto.coser@gmail.com>
2024-10-21 12:36:24 -0300

    Adição páginas Shared e adição de details de movies

--- AulaRazorPages/AulaRazorPages/Pages/Movies/Index.cshtml.cs
+++ AulaRazorPages/AulaRazorPages/Pages/Movies/Index.cshtml.cs
@@ -1,15 +1,20 @@
 using AulaRazorPages.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
-namespace AulaRazorPages.Pages
+namespace AulaRazorPages.Pages.Movies
 {
-    public class MoviesModel(MoviesContext moviesContext) : PageModel
+    public class IndexModel(MoviesContext moviesContext) : PageModel
     {
         public ICollection<Movie> Movies { get; private set; }
         public void OnGet()
         {
             Movies = moviesContext.Movies.ToList();
         }
+        //public async Task OnGetAsync()
+        //{
+        //    Movies = await moviesContext.Movies.ToListAsync();
+        //}
     }
 }
