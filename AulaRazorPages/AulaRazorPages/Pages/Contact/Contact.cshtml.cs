32d7e76202071411a40ab126de8ec33f2bf5f37e
Anacleto De Angeli Coser <anacleto.coser@gmail.com>
2024-10-21 23:05:09 -0300

    Criação page Contact

---
+++ AulaRazorPages/AulaRazorPages/Pages/Contact/Contact.cshtml.cs
@@ -0,0 +1,35 @@
+using AulaRazorPages.Records;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace AulaRazorPages.Pages.Contact
+{
+    public class ContactModel : PageModel
+    {
+        // Utilizando este BindProperty, não precisa passar o ContactFormModel como paarâmetro [FromForm] no método post
+        [BindProperty]
+        public ContactFormModel? ContactForm { get; set; } 
+        public void OnGet()
+        {
+            ContactForm = new ContactFormModel();
+        }
+
+        public IActionResult OnPost(
+             ) 
+        {
+            if (!ModelState.IsValid)
+            {
+                // Neste caso, não precisa da instrução abaixo, pois o BindProperty já está 'bindando' o ContactForm vindos lá do form do front
+                //ContactForm = formModel; 
+                return Page();
+            }
+
+            // send email
+            // save on database
+            // do something else
+
+            return Page();
+        }
+    }
+}
---
+++ AulaRazorPages/AulaRazorPages/Records/ContactFormModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AulaRazorPages.Records
+{
+    public record ContactFormModel
+    {
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(200)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Message { get; set; }
+    }
+}
